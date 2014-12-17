22c7239670d782a56c12c268314e8135c04d1b25
石井嗣 <YuZ@ishii-no-MacBook-Air.local>
2014-12-17 14:12:57 +0900

    Initial Commit

---
+++ goHomeLite/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  goHomeLite
+//
+//  Created by 石井嗣 on 2014/12/17.
+//  Copyright (c) 2014年 YuZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ goHomeLite/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  goHomeLite
+//
+//  Created by 石井嗣 on 2014/12/17.
+//  Copyright (c) 2014年 YuZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
